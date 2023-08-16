d491a1e6e0a982846cc11d04d670f67deb4db14d
wangding <408542507@qq.com>
2023-08-16 08:26:35 +0800

    modify mysql demo db setup

--- 06-mysql/03-setup-demo-db.sh
+++ 06-mysql/03-setup-demo-db.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-mysql -h 127.0.0.1 -u root -p < ./03-demo-db/schema.sql
-mysqlimport -h 127.0.0.1 -u root -p --local demo ./03-demo-db/areas.data
-mysqlimport -h 127.0.0.1 -u root -p --local demo ./03-demo-db/tags.data
-mysqlimport -h 127.0.0.1 -u root -p --local demo ./03-demo-db/authors.data
-mysqlimport -h 127.0.0.1 -u root -p --local demo ./03-demo-db/books.data
-mysqlimport -h 127.0.0.1 -u root -p --local demo ./03-demo-db/author_books.data
+mysql -h 127.0.0.1 -u root -pddd < ./03-demo-db/schema.sql
+mysqlimport -h 127.0.0.1 -u root -pddd --local demo ./03-demo-db/areas.data
+mysqlimport -h 127.0.0.1 -u root -pddd --local demo ./03-demo-db/tags.data
+mysqlimport -h 127.0.0.1 -u root -pddd --local demo ./03-demo-db/authors.data
+mysqlimport -h 127.0.0.1 -u root -pddd --local demo ./03-demo-db/books.data
+mysqlimport -h 127.0.0.1 -u root -pddd --local demo ./03-demo-db/author_books.data
